0336812dd0c415312c98da42e3866dac0d33337e
Pablo Garay <elxebec@gmail.com>
2025-08-30 18:41:40 -0300

    Create Libro.java

---
+++ 03 Introduccion a POO/Libro.java
@@ -0,0 +1,39 @@
+public class Libro {
+    private String titulo;
+    private String autor;
+    private int anioPublicacion;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        if (anioPublicacion > 0) { // Validación simple
+            this.anioPublicacion = anioPublicacion;
+        } else {
+            System.out.println("Año de publicación inválido.");
+        }
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Año de Publicación: " + anioPublicacion);
+    }
+}
