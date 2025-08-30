40832a38e4a96e9ded1858b4a5db1967a20d8153
Pablo Garay <elxebec@gmail.com>
2025-08-30 18:41:58 -0300

    Create Mascota.java

---
+++ 03 Introduccion a POO/Mascota.java
@@ -0,0 +1,16 @@
+public class Mascota {
+    String nombre;
+    String especie;
+    int edad;
+
+    public void mostrarInfo() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Especie: " + especie);
+        System.out.println("Edad: " + edad + " años");
+    }
+
+    public void cumplirAnios() {
+        edad++;
+        System.out.println(nombre + " ha cumplido " + edad + " años!");
+    }
+}
